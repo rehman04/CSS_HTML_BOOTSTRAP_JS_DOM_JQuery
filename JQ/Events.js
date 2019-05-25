80b0cea0b313c5b2ac98dd22e323780aba24a928
rehman04 <shifajamali55@gmail.com>
2019-05-25 14:21:35 +0500

    JQuery Basics

---
+++ JQ/Events.js
@@ -0,0 +1,21 @@
+// $("input").eq(0).keypress(function (event){
+// if (event.which===13){
+//   $('h3').toggleClass('turnBlue')}
+// })
+
+// on method
+// $('h1').on('dblclick',function(){
+//   $(this).toggleClass('turnBlue')
+// })
+
+// on hover
+$('h1').on('mouseenter',function(){
+  $(this).toggleClass('turnBlue')
+})
+
+// $('input').eq(1).on('click',function(){
+//   $('.container').fadeOut(3000)
+// })
+$('input').eq(1).on('click',function(){
+  $('.container').slideUp(3000)
+})
